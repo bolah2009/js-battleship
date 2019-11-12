4b23569b4def06c7f7ee3456fe54124166188c4a
Bola Ahmed Buari <bola_ahmed2002@yahoo.com>
2019-11-12 06:29:23 +0100

    Create `player` function
    The player function returns an object with the following property and
     methods:
    * `placeShips` method fills the board with ships either by generating
     random ships and positions or passing a list of valid ships.
       It returns true when a board is filled with ships otherwise, it returns false.
    * `attackedPositions` store the list of all played positions
    * `generateRandomPosition`  generates a valid position for the computer
     player which does not include already played positions.
    * `reset` takes a new board function and replace with the old board.
     It also deletes all attackedPositions values.
    
    TODO:
    generateRandomPosition should generate positions that are adjacent
     after getting a hit

--- test/ship.test.js
+++ test/ship.test.js
@@ -1,9 +1,10 @@
-import ship from '../src/modules/ship';
+import ship from '../src/modules/models/ship';
 
-const battleshipShip = ship('battleship', ['A1', 'A2', 'A3', 'A4']);
+const carrierShip = ship('carrier', ['A1', 'A2', 'A3', 'A4', 'A5']);
+const battleshipShip = ship('battleship', ['C1', 'C2', 'C3', 'C4']);
 const cruiserShip = ship('cruiser', ['G1', 'H1', 'I1']);
-const destroyerShip = ship('destroyer', ['C1', 'C2']);
-const submarineShip = ship('submarine', ['A1']);
+const submarineShip = ship('submarine', ['G4', 'G5', 'G6']);
+const destroyerShip = ship('destroyer', ['E1', 'E2']);
 
 test('it returns an object', () => {
   expect(typeof destroyerShip).toBe('object');
@@ -14,12 +15,12 @@ test('it includes size of the ship', () => {
 });
 
 describe('types of ships', () => {
-  test('the submarine is a single-decker - single-funnel with size 1', () => {
-    expect(submarineShip.size).toBe(1);
-  });
-
   test('the destroyer is a two-decker - two-funnel with size 2', () => {
     expect(destroyerShip.size).toBe(2);
+  });
+
+  test('the submarine is a three-decker - three-funnel with size 3', () => {
+    expect(submarineShip.size).toBe(3);
   });
 
   test('the cruiser is a three-decker - three-funnel with size 3', () => {
@@ -29,17 +30,21 @@ describe('types of ships', () => {
   test('the battleship is a four-decker - four-funnel with size 4', () => {
     expect(battleshipShip.size).toBe(4);
   });
+
+  test('the carrier is a five-decker - five-funnel with size 5', () => {
+    expect(carrierShip.size).toBe(5);
+  });
 });
 
 test('it includes positions of where it has been hit', () => {
-  const hitPosition = 'C1';
+  const hitPosition = 'E1';
   destroyerShip.hit(hitPosition);
   expect(destroyerShip.hits).toContain(hitPosition);
 });
 
 describe('hit() method', () => {
   test('takes a string as position returns true if the ship is in the same position', () => {
-    expect(battleshipShip.hit('A1')).toBeTruthy();
+    expect(battleshipShip.hit('C1')).toBeTruthy();
   });
 
   test('returns false if the ship is not in the same position', () => {
@@ -47,15 +52,16 @@ describe('hit() method', () => {
   });
 
   test('returns false if the ship has been hit in the same position', () => {
-    destroyerShip.hit('C1');
-    expect(destroyerShip.hit('C1')).toBeFalsy();
+    destroyerShip.hit('E1');
+    expect(destroyerShip.hit('E1')).toBeFalsy();
   });
 });
 
 describe('isSunk() method', () => {
   test('returns true if all position of the ship has been hit', () => {
-    submarineShip.hit('A1');
-    expect(submarineShip.isSunk()).toBeTruthy();
+    destroyerShip.hit('E1');
+    destroyerShip.hit('E2');
+    expect(destroyerShip.isSunk()).toBeTruthy();
   });
 
   test('returns false if one or more of the ship position have not been hit', () => {
